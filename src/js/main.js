f21f99985327821106c1d675400d07820be36eb7
Shay <hallshay@gmail.com>
2015-10-17 14:06:08 -0400

    Refactor from the ground up.

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,28 @@
-// jQuery.ajax('../../apis/github/users/octocat.json').then(function(data){
-//  console.log(arguments);
-// });
-
 jQuery.ajax("../apis/github/users/shayhall.json").then(function(data){
  console.log(arguments);
 });
 
+jQuery.ajax("../apis/github/users/shayhall/repos.json").then(function(data){
+ console.log(arguments);
+});
+
+jQuery.ajax("../apis/github/users/shayhall/activity.json").then(function(data){
+ console.log(arguments);
+});
+
+$('.tabs a[href]').on('click', function(event){
+  event.preventDefault();
+    $(this).add(this.hash)
+    .addClass('active')
+    .siblings().removeClass('active');
+});
+
+$('.mobile a[href]').on('click', function(event){
+  event.preventDefault();
+    $(this).add(this.hash)
+    .addClass('active')
+    .siblings().removeClass('active');
+});
 
 var info = jQuery.getJSON("../apis/github/users/shayhall.json", function(data) {
   $('#username').html('<h2>' + data.name);
@@ -13,18 +30,6 @@ var info = jQuery.getJSON("../apis/github/users/shayhall.json", function(data) {
   $('.company').html('<li>' + data.company);
   $('.location').html('<li>' + data.location);
   $('.email').html('<li>' + data.email);
+  $('.blog').html('<li>' + data.blog);
+  $('.created_at').html('<li>' + data.created_at);
 });
-
-
-
-$(".tabs").click(function(){
-    addTabs();
-    $(this).addClass("active");
-    $(this).next().addClass("active");
-    $(this).next().css("display", "block");
-}); //Adapted from http://www.jankoatwarpspeed.com/dynamic-tabs-using-jquery-why-and-how-to-create-it/
-
-function addTabs() {
-  $(".tabs").removeClass("active");
-  $(".tabs").next().css("display", "none");
-}; // Used within event listener to move active class
