7c0df2bcde24f48bd66fe8e4c1f49f66ce04051b
Shay <hallshay@gmail.com>
2015-10-21 22:07:23 -0400

    Add IIFE for next part.

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,3 @@
-//
-// jQuery.ajax("../apis/github/users/shayhall.json").then(function(data){
-//  console.log(arguments);
-// });
-//
-// jQuery.ajax("../apis/github/users/shayhall/repos.json").then(function(data){
-//  console.log(arguments);
-// });
-//
-// jQuery.ajax("../apis/github/users/shayhall/activity.json").then(function(data){
-//  console.log(arguments);
-// });
-
 $('.tabs a[href]').on('click', function(event){
   event.preventDefault();
     $(this).add(this.hash)
@@ -25,16 +12,6 @@ $('.mobile a[href]').on('click', function(event){
     .siblings().removeClass('active');
 });
 
-// var info = jQuery.getJSON("../apis/github/users/shayhall.json", function(data) {
-//   $('#username').html('<h2>' + data.name);
-//   $('#login').html('<h2>' + data.login);
-//   $('.company').html('<li>' + data.company);
-//   $('.location').html('<li>' + data.location);
-//   $('.email').html('<li>' + data.email);
-//   $('.blog').html('<li>' + data.blog);
-//   $('.created_at').html('<li>' + data.created_at);
-//   $('avatar_url').html('<a>' + data.avatar_url);
-// });
 ;(function(){
 
 angular.module('Github', [])
@@ -62,5 +39,13 @@ angular.module('Github', [])
   //   $http.get("../apis/github/users/shayhall/activity.json")
   //     .then(function(activity){
   //       $rootScope.shayhall = activity.data;
+
+})(); //IIFE
+
+;(function(){
+
+
+
+
 
 })(); //IIFE
